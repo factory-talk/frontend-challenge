f9f9e0d55a64b41ce3829e79372d71b0edb710ec
NNplatform <suchawadeenuna@gmail.com>
2024-09-07 15:22:29 +0700

    fix: add logic fetch country detail with zipcode in usecase

--- src/application/useCases/CityUseCases.ts
+++ src/application/useCases/CityUseCases.ts
@@ -1,5 +1,7 @@
 import { OpenWeatherGeocodingService } from '@application/services/OpenWeatherGeocodingService';
 import { CityViewModel } from '@application/dtos/CityViewModel';
+import { Logger } from '@infrastructure/utils/Logger';
+
 
 export class CityUseCases {
   private geocodingService: OpenWeatherGeocodingService;
@@ -9,8 +11,45 @@ export class CityUseCases {
   }
 
   public async searchCityOrZip(query: string): Promise<CityViewModel[]> {
-    const cities = await this.geocodingService.searchCity(query);
-    return cities.map(city => new CityViewModel(city));
+    let results: CityViewModel[] = [];
+
+    try {
+      // Search by city name
+      const cityResults = await this.geocodingService.searchCity(query);
+      results = cityResults.map(dto => new CityViewModel(dto));
+
+      // Search by ZIP code
+      try {
+        const zipResult = await this.geocodingService.searchByZip(query);
+        Logger.log('zipResult: ', zipResult);
+
+        if (zipResult) {
+          zipResult.map(dto => results.push(new CityViewModel(dto)));
+        }
+      } catch (error) {
+        Logger.logError('Error in searchByZip: ',null)
+      }
+      const uniqueResults = this.removeDuplicates(results);
+      Logger.log("uniqueResults: ",uniqueResults)
+
+      return uniqueResults;
+    } catch (error) {
+      console.error('Error in searchCityOrZip: ', error);
+      throw error;
+    }
+  }
+
+  private removeDuplicates(cities: CityViewModel[]): CityViewModel[] {
+    const uniqueCities = new Map<string, CityViewModel>();
+    
+    for (const city of cities) {
+      const key = `${city.getLatitude()},${city.getLongitude()}`;
+      if (!uniqueCities.has(key)) {
+        uniqueCities.set(key, city);
+      }
+    }
+
+    return Array.from(uniqueCities.values());
   }
 
 }
