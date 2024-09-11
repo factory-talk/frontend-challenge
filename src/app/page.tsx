8de922fe8bcf657bef6fda04f17578de46aa80c3
Patorn Kwanthale <the_dann@hotmail.co.th>
2024-09-12 03:15:11 +0700

    feat: link detail page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 import SearchBar from "src/components/SearchBar";
 import WeatherItemList from "src/components/WeatherItemList";
 import React, { useState } from "react";
-import ActiveSearchResponse from "src/interfaces/ActiveSearch";
+import ActiveSearchResponse from "src/interfaces/ActiveSearch/ActiveSearchResponse";
 
 
 export default function Home() {
   const [selecedtActiveSearch, setSelectedActiveSearch] = useState<ActiveSearchResponse>()
   return (
     <>
-      <SearchBar setSelectedActiveSearch={setSelectedActiveSearch}/>
+      <SearchBar setSelectedActiveSearch={setSelectedActiveSearch} />
       <WeatherItemList activeSearch={selecedtActiveSearch} />
     </>
   );
--- src/components/WeatherItem/WeatherItem.tsx
+++ src/components/WeatherItem/WeatherItem.tsx
@@ -1,16 +1,17 @@
+import Link from "next/link"
 import React from "react"
 import WeatherItemProp from "src/interfaces/WeatherList/WeatherItemProp"
 
 const WeatherItem = (weatherItemProps: WeatherItemProp) => {
     return (
-        <>
-            <div className="w-full grid grid-cols-4 grid-rows-2 py-2">
-                <div className="px-4">{weatherItemProps.cityDispayName}</div>
-                <div className="col-start-3 row-span-2 text-center">{weatherItemProps.weather[0].icon}</div>
-                <div className="row-span-2 text-center">{weatherItemProps.main.temp}</div>
+        <Link href="/detail">
+            <div className="w-full grid grid-cols-5 sm:grid-cols-12 grid-rows-2 py-2 border-b border-slate-600 hover:bg-gray-100 cursor-pointer">
+                <div className="px-4 col-span-2 sm:col-span-3 text-2xl md:text-4xl font-bold">{weatherItemProps.cityDispayName}</div>
+                <img className="max-h-none row-span-2 col-start-4 sm:col-start-11 flex items-center justify-center" src={`https://openweathermap.org/img/wn/${weatherItemProps.weather[0].icon}@2x.png`} />
+                <span className="row-span-2 text-3xl min-h-full flex items-center justify-center">{Math.ceil(weatherItemProps.main.temp)}°C</span>
                 <div className="px-4">{weatherItemProps.dt.getHours()}:{weatherItemProps.dt.getMinutes()}</div>
             </div>
-        </>
+        </Link>
 
     )
 }
