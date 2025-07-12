b4f2f7686612e550ae37aa7172e663d9518445ec
natthachai.s <natthachaisae@gosoft.co.th>
2025-07-12 18:10:58 +0700

    chore: setup next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_FORECAST_URL: process.env.NEXT_PUBLIC_FORECAST_URL,
+        NEXT_PUBLIC_LACATION_URL: process.env.NEXT_PUBLIC_LACATION_URL,
+        NEXT_PUBLIC_WEATHER_URL: process.env.NEXT_PUBLIC_WEATHER_URL
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'openweathermap.org',
+                port: '',
+                pathname: '/img/wn/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
