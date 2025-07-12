441f2efbc4a119da9db5a95e15870ab0271131f4
natthachai.s <natthachaisae@gosoft.co.th>
2025-07-12 18:26:13 +0700

    feat: hook getWeather data

---
+++ src/hooks/get-weather.tsx
@@ -0,0 +1,20 @@
+import axios from "axios"
+import { WeatherResp } from "@/interface/response/weather-resp"
+import { APP_CONFIG } from "@/config/app.config"
+
+interface GetWeatherProp {
+    lat: string
+    lon: string
+    units: string
+}
+
+export const getWeather = async (query: GetWeatherProp) => {
+    const url = APP_CONFIG.NEXT_PUBLIC_WEATHER_URL
+    const params = new URLSearchParams({
+        appid: APP_CONFIG.NEXT_PUBLIC_APPID || '07318e34fb59a790b30ba1a3caa8c851',
+        ...query
+    }).toString();
+
+    const result = await axios.get<WeatherResp>(`${url}?${params}`)
+    return result.data
+}
