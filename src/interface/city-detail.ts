951f4124ddb5337d6b990d113ced252490a8c8e8
natthachai.s <natthachaisae@gosoft.co.th>
2025-07-14 18:00:54 +0700

    type: ass CityDetail ,WeatherDetail

--- src/interface/city-detail.ts
+++ src/interface/city-detail.ts
@@ -1,11 +1,13 @@
 import { ForecastData } from "./response/forecast-resp"
-import { LocationResp } from "./response/location-resp"
 import { WeatherResp } from "./response/weather-resp"
 
-
-export type WeatherDetail = {
+export type CityDetail = {
     id: string
+    lat: string
+    lon: string
+    display_place: string
+    display_name: string
+    country_code: string
     weather: WeatherResp
     forecast: ForecastData[]
-    location: LocationResp
 }
