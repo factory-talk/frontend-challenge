5ea5dcff6c9216e78c780355470faf3d178f3213
natthachai.s <natthachaisae@gosoft.co.th>
2025-07-15 16:10:51 +0700

    feat: update unit in store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,22 +1,32 @@
 import { CityDetail } from '@/interface/city-detail'
+import { Units } from '@/interface/units'
 import { WeatherDetail } from '@/interface/weather-detail'
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
+
+
 type State = {
     cityList: CityDetail[]
     weatherDetail?: WeatherDetail
+    units: Units
+    fetchKey: string
 }
 
 type Actions = {
     setWeatherDetail: (weatherDetails: WeatherDetail) => void
     setCityList: (city: CityDetail) => void
     deleteCityList: (id: string) => void
+    setUnits: (units: Units) => void
+    setFetchKey: () => void
+    setCityListAll: (cities: CityDetail[]) => void
 }
 
 const initialState: State = {
     cityList: [],
-    weatherDetail: undefined
+    weatherDetail: undefined,
+    units: 'metric',
+    fetchKey: ''
 }
 
 export const useValueStore = create<State & Actions>()(
@@ -37,6 +47,13 @@ export const useValueStore = create<State & Actions>()(
                     if (isExist) return state
                     return { cityList: [...state.cityList, value] }
                 }),
+                setUnits: (value) => set(() => {
+                    return { units: value }
+                }),
+                setFetchKey: () => set(() => ({ fetchKey: Date.now().toString() })),
+                setCityListAll: (cities) => set(() => ({
+                    cityList: cities
+                }))
             }),
             {
                 name: 'city-store',
