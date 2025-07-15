1474768e6c10b2240c951c0220d2bd72f4b2ae9e
natthachai.s <natthachaisae@gosoft.co.th>
2025-07-15 16:13:32 +0700

    feat: adjust LayoutWrapper style ,add Home Button

--- src/components/layout/LayoutWrapper.tsx
+++ src/components/layout/LayoutWrapper.tsx
@@ -4,20 +4,30 @@
 import { PropLayout } from "@/interface/prop-layout"
 import { AntdProvider } from "@/provider/AntdProvider"
 import SearchBox from "../tools/SearchBox"
+import UnitsToggle from "../tools/UnitsToggle"
+import { HomeOutlined } from "@ant-design/icons";
+import { Button } from "antd"
+import { useRouter } from "next/navigation"
+
 
 function LayoutWrapper({ children }: PropLayout) {
+    const router = useRouter()
 
     return (
         <html lang="en">
             <body>
                 <AntdProvider>
-                    <div className="min-h-screen bg-gradient-to-br from-blue-400 via-blue-300 to-blue-400">
+                    <div className="min-h-screen bg-gradient-to-br from-blue-500 via-blue-400 to-indigo-500">
                         <div className="container mx-auto px-4 py-8">
                             <div className="text-center mb-8">
                                 <h1 className="text-4xl font-bold text-white mb-2">Weather & Forecast App</h1>
-                                <p className="text-blue-100">Get current weather and 24-hour forecast</p>
-                                <div className="py-8">
+                                <p className="text-white">Get current weather and 24-hour forecast</p>
+                                <div className="py-4">
+                                    <Button icon={<HomeOutlined />} shape="circle" type="default" size='large' onClick={() => router.push('/')} className="mr-2 mb-2" />
                                     <SearchBox />
+                                </div>
+                                <div className="py-4 flex justify-center ">
+                                    <UnitsToggle />
                                 </div>
                             </div>
                             <div>
